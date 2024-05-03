78795d36f61fe7687381904db31f350f4df5ebce
vizonIT <osukakvick@gmail.com>
2024-05-04 00:20:50 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(1);
+
+  function handleIncreaseStep() {
+    setStep((initStep) => initStep + 1);
+  }
+  const handleReduceStep = () => {
+    step > 1 && setStep((initStep) => initStep - 1);
+  };
+
+  function handleIncreaseCount() {
+    setCount((initCount) => initCount + 1);
+  }
+  const handleReduceCount = () => {
+    setCount((initCount) => initCount - 1);
+  };
+  const updatedCount = count + step;
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+  // console.log(date);
+  return (
+    <div className="App">
+      <div className="stepContainer">
+        <button onClick={handleReduceStep}>-</button>
+        <p>Step: {step}</p>
+        <button onClick={handleIncreaseStep}>+</button>
+      </div>
+      <div className="countContainer">
+        <button onClick={handleReduceCount}>-</button>
+        <p>Count: {updatedCount}</p>
+        <button onClick={handleIncreaseCount}>+</button>
+      </div>
+      <div>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} day(s) from today will be `
+            : `${Math.abs(count)} day(s) ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
